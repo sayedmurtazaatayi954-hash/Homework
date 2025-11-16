9ad8dfe0580e3163a9cf549694aba17391a4ae21
sayedmurtazaatayi954-hash <sayedmurtazaatayi954@gmail.com>
2025-11-16 12:34:23 +0430

    Add files via upload

---
+++ months.java
@@ -0,0 +1,44 @@
+public class Main {
+	public static void main(String[] args) {
+		int month;
+		for(month=1;month<=12;month++){
+		    switch(month){
+		        case 1:
+		         System.out.println("janvary");
+		         break;
+		         case 2:
+		          System.out.println("fabervary");
+		          break;
+		          case 3:
+		           System.out.println("march");
+		           break;
+		           case 4:
+		            System.out.println("april");
+		            break;
+		            case 5:
+		             System.out.println("may");
+		             break;
+		             case 6:
+		              System.out.println("jun");
+		              break;
+		              case 7:
+		               System.out.println("july");
+		               break;
+		               case 8:
+		                System.out.println("agust");
+		                break;
+		                case 9:
+		                 System.out.println("september");break;
+		                 case 10:
+		                  System.out.println("october");break;
+		                  
+		            case 11:
+		             System.out.println("nevember");
+		             break;
+		             case 12:
+		              System.out.println("december");
+		              break;      
+	}
+}
+	}
+}
